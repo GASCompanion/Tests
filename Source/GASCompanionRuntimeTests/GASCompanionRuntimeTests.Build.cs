4d125dcb8b7e7d8e6d2c7bd330bbd1bd98d1d2f8
Mickael Daniel <daniel.mickael@gmail.com>
2022-05-01 20:50:27 +0200

    Tests: Add AnimInstance functional tests (for ASC on ACharacter, AGSCModularCharacter and AGSCModularPlayerStateCharacter)

--- Source/GASCompanionRuntimeTests/GASCompanionRuntimeTests.Build.cs
+++ Source/GASCompanionRuntimeTests/GASCompanionRuntimeTests.Build.cs
@@ -43,6 +43,7 @@ public class GASCompanionRuntimeTests : ModuleRules
 				"SlateCore",
 				"GASCompanion",
 				"EnhancedInput",
+				"GameplayTags"
 			}
 		);
 
---
+++ Source/GASCompanionRuntimeTests/Public/GASCompanionTestsNativeTags.h
@@ -0,0 +1,29 @@
+﻿// Copyright 2022 Mickael Daniel. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "GameplayTagsManager.h"
+
+// ReSharper disable once CppPolymorphicClassWithNonVirtualPublicDestructor
+struct GASCOMPANIONRUNTIMETESTS_API FGASCompanionTestsNativeTags : public FGameplayTagNativeAdder
+{
+	FGameplayTag EventLanded;
+	FGameplayTag AbilityJump;
+
+	FORCEINLINE static const FGASCompanionTestsNativeTags& Get() { return NativeTags; }
+
+	virtual void AddTags() override
+	{
+		UGameplayTagsManager& Manager = UGameplayTagsManager::Get();
+
+		EventLanded = Manager.AddNativeGameplayTag(TEXT("GASCompanionTests.Event.Landed"));
+		AbilityJump = Manager.AddNativeGameplayTag(TEXT("GASCompanionTests.Ability.Jump"));
+	}
+
+private:
+
+	static FGASCompanionTestsNativeTags NativeTags;
+
+};
