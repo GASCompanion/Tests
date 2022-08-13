81b1e5bca486b1d7a7df7064b7174aa3eb42133d
Mickael Daniel <daniel.mickael@gmail.com>
2022-08-13 19:05:26 +0200

    Setup node scripts and workflow

---
+++ generate-html.js
@@ -0,0 +1,51 @@
+
+const fs = require("fs");
+const path = require("path");
+
+const dirs = fs.readdirSync("./builds", { withFileTypes: true })
+    .filter((item) => item.isDirectory())
+    .filter((item) => !item.name.startsWith("."))
+    .map((item) => {
+        const time = fs.statSync(item.name).mtime.getTime();
+        const jsonPath = path.join(__dirname, item.name, "index.json");
+        const context = fs.existsSync(jsonPath) ? require("./" + item.name + "/index.json") : {};
+        const { reportCreatedOn, failed, succeededWithWarnings  } = context;
+        const testState = failed > 0 ? "danger" : succeededWithWarnings > 0 ? "attention" : "success";
+
+        let date =  new Date();
+        
+        if (reportCreatedOn) {
+            date = new Date(`${reportCreatedOn.split("-")[0].replace(/\./g, "-")}T${reportCreatedOn.split("-")[1].replace(/\./g, ":")}.000Z`);
+        }
+
+        return { item, time, date, testState };
+    })
+    .sort((left, right) => right.date.getTime() - left.date.getTime() );
+
+const html = `<!doctype html>
+<html lang="en" data-color-mode="light" data-dark-theme="light">
+    <head>
+        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="stylesheet" href="https://unpkg.com/@primer/css@^19.0.0/dist/primer.css"/>
+    </head>
+    <body>
+
+        <div class="container-xl px-md-6 pt-12">
+            <div class="Subhead">
+                <h2 class="Subhead-heading">Test Reports</h2>
+            </div>
+
+            ${dirs.map(({ item, time, date, testState }) => `
+                <p>
+                    <a class="no-underline" href="${item.name}/index.html">${item.name}</a> (<a href="${item.name}/index.json">json</a>)
+                    <span class="Label Label--${testState}">${date}</span>
+                </p>
+            `).join("\n")}
+        </div>
+
+    </body>
+</html>
+`;
+
+console.log(html);
---
+++ generate-readme.js
@@ -0,0 +1,39 @@
+
+const fs = require("fs");
+const path = require("path");
+
+const dirs = fs.readdirSync("./builds", { withFileTypes: true })
+    .filter((item) => item.isDirectory())
+    .filter((item) => !item.name.startsWith("."))
+    .map((item) => {
+        const time = fs.statSync(item.name).mtime.getTime();
+        const jsonPath = path.join(__dirname, item.name, "index.json");
+        const context = fs.existsSync(jsonPath) ? require("./" + item.name + "/index.json") : {};
+        const { reportCreatedOn, failed, succeededWithWarnings  } = context;
+        const testState = failed > 0 ? "error" : succeededWithWarnings > 0 ? "warning" : "success";
+
+        let date =  new Date();
+        
+        if (reportCreatedOn) {
+            date = new Date(`${reportCreatedOn.split("-")[0].replace(/\./g, "-")}T${reportCreatedOn.split("-")[1].replace(/\./g, ":")}.000Z`);
+        }
+
+        return { item, time, date, testState }
+    })
+    .sort((left, right) => right.date.getTime() - left.date.getTime() );
+
+const html = `## Test Reports
+
+| Build | Json | Date | Status |
+| ----- | ---- | ---- | ------ |
+${dirs.map(({ item, time, date, testState }) => `
+| [${item.name}](${item.name}/index.html) | [index.json](${item.name}/index.json) | ${date ? date.toUTCString() : '' } | ${testState} |
+`).map(item => item.trim() ).join("\n")}
+
+---
+
+Generated on ${new Date().toUTCString()}
+
+`;
+
+console.log(html);
